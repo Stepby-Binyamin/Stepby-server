53e40478af2861bece984b1ae04fa14c994253ec
נהוראי <nehoraimimon@gmail.com>
2022-09-12 13:42:07 +0300

    swagger with some comments

---
+++ server.js
@@ -0,0 +1,26 @@
+require('dotenv').config({ path: "config/.env"})
+const swaggerFile =require("./swagger/swaggerOutput.json"),
+swaggerUi = require("swagger-ui-express")
+
+const express = require("express")
+// const bodyParser = require("body-parser")
+
+const cors = require("cors")
+const { config } = require('process')
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+PORT = process.env.PORT || 5001
+
+const mainRouter = require("./config/mainRoutes")
+app.use("/", mainRouter)
+
+require("./data/db").connect()
+
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
+
+app.listen(PORT, ()=> console.log(`Server is running at Port ${PORT}`))
