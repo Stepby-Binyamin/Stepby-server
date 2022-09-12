b0673314007e97ed90df45ec03176bded4add9ca
oriya <oriyaroth3@gmail.com>
2022-09-12 10:08:02 +0300

    First commit

--- config/mainRoutes.js
+++ config/mainRoutes.js
@@ -2,7 +2,8 @@ const express = require("express")
 const router = express.Router()
 
 const userRouter = require("../user/user.route")
-
+const abc = require('../auth/temp')
 router.use("/user", userRouter)
+router.use('/auth', abc)
 
 module.exports = router
--- user/user.route.js
+++ user/user.route.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const router = express.Router()
 
-const userController = require("./user.control")
+// const userController = require("./user.control")
 
 
 
