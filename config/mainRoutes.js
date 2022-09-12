7f9da15b2904755828ed4aae21ea7a7ac24b43a4
נהוראי <nehoraimimon@gmail.com>
2022-09-12 13:41:55 +0300

    swagger with some comments

--- config/mainRoutes.js
+++ config/mainRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const router = express.Router()
 
-const userRouter = require("../user/user.route")
+const userRouter = require("../user/user.route.js")
 
 /**
    * @openapi
--- user/user.route.js
+++ user/user.route.js
@@ -2,19 +2,13 @@ const express = require("express")
 const router = express.Router()
 
 // const userController = require("./user.control")
-/**
-   * @openapi
-   * /all:
-   *  get:
-   *     tags:
-   *     - all
-   *     description: Responds if the app is up and running
-   *     responses:
-   *       200:
-   *         description: App is up and running
-   */
-router.use("/all", (req , res)=>{
+
+router.get("/all", (req , res)=>{
+    // #swagger.summary = 'summary'
+    // #swagger.tags = ['Users']
+    // #swagger.description = 'Some description...'
     console.log("nehorai");
+    
     res.send("Welcome")
 })
 
