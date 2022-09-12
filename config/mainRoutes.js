6206b40eb5c3cf0496eb4e6d3c7bcae8ec5ce502
נהוראי <nehoraimimon@gmail.com>
2022-09-12 10:24:15 +0300

    bug fix

--- config/mainRoutes.js
+++ config/mainRoutes.js
@@ -3,7 +3,17 @@ const router = express.Router()
 
 const userRouter = require("../user/user.route")
 
-
+/**
+   * @openapi
+   * /user:
+   *  get:
+   *     tags:
+   *     - user
+   *     description: Responds if the app is up and running
+   *     responses:
+   *       200:
+   *         description: App is up and running
+   */
 router.use("/user", userRouter)
 
 
