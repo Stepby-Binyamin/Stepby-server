7361cdebca7e4d61445caaf8b7aa25df5b66a085
נהוראי <nehoraimimon@gmail.com>
2022-09-13 11:18:13 +0300

    more data to swagger

--- config/mainRoutes.js
+++ config/mainRoutes.js
@@ -5,6 +5,7 @@ const userRouter = require("../user/user.route")
 const templateRouter = require("../template/template.route")
 const categoryRouter = require('../category/category.routes')
 const languageRouter = require('../language/language.route')
+const projectRoute = require('../template/project.route')
 const abc = require('../auth/temp')
 
 
@@ -13,6 +14,7 @@ router.use("/template", templateRouter)
 router.use('/auth', abc)
 router.use('/category', categoryRouter)
 router.use('/language', languageRouter)
+router.use('/project', projectRoute)
 
 
 
