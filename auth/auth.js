47d70e45923d7473e6dfa8e720a8603e41c12968
yosefus <yosalsoni@gmail.com>
2022-09-13 12:50:48 +0300

    text changes

--- auth/auth.js
+++ auth/auth.js
@@ -5,7 +5,8 @@ const { readOne } = require("../user/user.model");
 const authJWT = async (req, res, next) => {
     const newToken = jwt.sign({ _id: "631f535f76a1bb19c3e0e309" }, process.env.JWT_SECRET, { expiresIn: "10h" })
     //TODO: delete --- || `Bearer ${newToken}`; ---  before production.
-    const authHeader = req.headers.authorization || `Bearer ${newToken}`; 
+
+    const authHeader = req.headers.authorization || `Bearer ${newToken}`;
 
     if (authHeader) {
         const token = authHeader.split(" ")[1];
