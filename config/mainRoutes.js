64f7e46415aa0f20f3ea54c8680f96e67adc17ce
נהוראי <nehoraimimon@gmail.com>
2022-09-13 11:42:05 +0300

    finish swagger besic level

--- config/mainRoutes.js
+++ config/mainRoutes.js
@@ -6,12 +6,10 @@ const templateRouter = require("../template/template.route")
 const categoryRouter = require('../category/category.routes')
 const languageRouter = require('../language/language.route')
 const projectRoute = require('../template/project.route')
-const abc = require('../auth/temp')
 
 
 router.use("/user", userRouter)
 router.use("/template", templateRouter)
-router.use('/auth', abc)
 router.use('/category', categoryRouter)
 router.use('/language', languageRouter)
 router.use('/project', projectRoute)
