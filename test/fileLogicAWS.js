9470b24ead0d8d0442e21aac8153d05721fb4c46
Shaul Attie <solyattie13@gmail.com>
2022-09-13 17:33:07 +0300

    17:30

--- test/fileLogicAWS.js
+++ test/fileLogicAWS.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 
-const AWS = require('aws-sdk')
+const AWS = require('aws-sdk');
 
 const s3 = new AWS.S3({
     accessKeyId: process.env.AWS_S3_ACCESS_KEY_ID,
@@ -8,55 +8,65 @@ const s3 = new AWS.S3({
 })
 
 
-async function createFile(fileName = "", dataContent = "", step, description, project, shortQuestion, enconding = "utf-8") {
+async function createFile(fileName = "", dataContent = "", shortQuestion, data, enconding = "utf-8") {
     // console.log("fileLogic", fileName, dataContent, step, description, project);
-    console.log("13",shortQuestion);
 
+    console.log("fileName", fileName);
+    console.log("dataContent", dataContent);
+    console.log("data", data);
 
-    if (!fs.existsSync(`${project}/${step}`)) {
-        fs.mkdirSync(`${project}`)
-        fs.mkdirSync(`${project}/${step}`)
+    shortQuestion00 = JSON.parse(shortQuestion)
+    // console.log("13", shortQuestion00);
+
+    if (!fs.existsSync(`${shortQuestion00.project}/${shortQuestion00.step}`)) {
+        fs.mkdirSync(`${shortQuestion00.project}`)
+        fs.mkdirSync(`${shortQuestion00.project}/${shortQuestion00.step}`)
     }
 
-    if (fs.existsSync(`${project}/${step}/description.txt`)) {
-        console.log("The File exists");
+    if (fs.existsSync(`${shortQuestion00.project}/${shortQuestion00.step}/description.txt`)) {
+        // console.log("The File exists");
     }
     try {
         // Create
         let logs = []
-        if (fs.existsSync(`${project}/${step}/description.txt`)) {
-            logs = fs.readFileSync(`${project}/${step}/description.txt`).toString().split("\n");
+        if (fs.existsSync(`${shortQuestion00.project}/${shortQuestion00.step}/description.txt`)) {
+            logs = fs.readFileSync(`${shortQuestion00.project}/${shortQuestion00.step}/description.txt`).toString().split("\n");
             shortQuestion = `\n${shortQuestion}`
             logs.push(shortQuestion)
-            console.log(logs);
-            fs.writeFileSync(`${project}/${step}/description.txt`, `${logs}`)
+            fs.writeFileSync(`${shortQuestion00.project}/${shortQuestion00.step}/description.txt`, `${logs}`)
         } else {
-            fs.writeFileSync(`${project}/${step}/description.txt`, shortQuestion)
+            fs.writeFileSync(`${shortQuestion00.project}/${shortQuestion00.step}/description.txt`, shortQuestion)
         }
         if (fileName) {
-            fs.writeFileSync(`${project}/${step}/${fileName}`, dataContent, { enconding });
+            fs.writeFileSync(`${shortQuestion00.project}/${shortQuestion00.step}/${fileName}`, dataContent, { enconding });
         }
 
-        // const fileContent = fs.readFileSync(`${project}/${step}/description.txt`)
-        // const uploadTxt = await s3.upload({
-        //     Bucket: process.env.AWS_BUCKET_NAME,
-        //     Key: "description.txt",
-        //     Body: fileContent,
-        // }).promise()
+        // const params = {
+        //     Bucket: "NewName",
+        //     CreateBucketConfiguration: {
+        //         LocationConstraint: "eu-west-3",
 
-        const blob = fs.readFileSync(`${project}/${step}/${fileName}`)
-        const uploadedImage = await s3.upload({
+        //     }
+        // };
+        // const newBucket = s3.createBucket(params, function (err, data) {
+        //     if (err) console.log(err, err.stack);
+        //     else console.log('Bucket Created Successfully', data.Location);
+        // });
+
+        // newBucket
+
+        const blob = dataContent
+        const uploadedFile = await s3.upload({
             Bucket: process.env.AWS_S3_BUCKET_NAME,
             Key: fileName,
             Body: blob,
         }).promise()
 
-        uploadedImage
-        // uploadTxt
+        uploadedFile
 
         // console.log(uploadedImage.Location);
 
-        return { filepath: `file created at ${project}/${step}/${fileName}`, uploadLocation: uploadedImage.Location };
+        return { filepath: `file created at ${shortQuestion00.project}/${shortQuestion00.step}/${fileName}`, uploadLocation: uploadedImage.Location };
     } catch (error) {
         return "Error from existFileCreate";
     }
--- test/shaul.js
+++ test/shaul.js
@@ -9,11 +9,11 @@ const fileLogic = require("./fileLogicAWS")
 
 router.post('/files/upload', upload.single("new_file"), async (req, res) => {
 
-console.log("111",req.body.objShortQuestion);
+    console.log("111", req.body.data);
 
     try {
         const { file, body } = req
-        const result = await fileLogic.createFile(file?.originalname, file?.buffer, body.step, body.description, body.project, body.objShortQuestion)
+        const result = await fileLogic.createFile(file?.originalname, file?.buffer, body.objShortQuestion, body.data)
 
         res.status(200).send(result)
     } catch (err) {
